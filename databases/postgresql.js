a419fd572d0e32cfc32a3aac700b56eb10e46290
unknown <sebastian.granados.barrantes@gmail.com>
2025-07-21 10:47:16 -0600

    [ADD] New table transaction

--- databases/postgresql.js
+++ databases/postgresql.js
@@ -4,7 +4,9 @@ const postgresql = newDb();
 
 // create mock data
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
-                insert into users values (123, 'Juan', true);`);
+		create table transaction( pk_transaction integer, fk_user integer, description text, amount real);
+                insert into users values (123, 'Juan', true);
+		insert into users values (1, 'Sebastián', true);`);
 
 
 module.exports = {
